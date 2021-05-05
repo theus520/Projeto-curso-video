aecc785a3a63dce60c8f5474dde9d53da942b17d
matheus rabelo <matheusrabelo520@gmail.com>
2021-05-04 21:28:37 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const footer  = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido By <a href="https://github.com/theus520" target="_blank" rel="noreferrer">
+        Matheus Rabelo Sousa Cezario Leite</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+  </div>
+</footer>
+
+    );
+}
+
+export default footer;
