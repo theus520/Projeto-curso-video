e3d1739f68bf6b8e869b516932a5463931ce2140
matheus rabelo <matheusrabelo520@gmail.com>
2021-05-09 20:07:29 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
@@ -6,7 +7,7 @@ import { BASE_URL } from "utils/request";
 
 const DataTable = () => {
    
-   
+const [activePage, setActivePage] = useState(0);
     const [page, setPage] = useState <SalePage>({
         first : true,
         last: true,
@@ -14,21 +15,23 @@ const DataTable = () => {
         totalElements: 0,
         totalPages : 0
     });
-   
-
-
  
-
     useEffect(()=>{
-      
-        axios.get(`${BASE_URL}/sales?page=1&size=10&sort=date,desc`)
+    
+        axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
         .then(response => {
             setPage(response.data)
-        })
+        });
 
-    }, []);
+    }, [activePage]);
    
+const changePage = (index: number) =>{
+    setActivePage(index);
+}
+
     return (
+        <>
+        <Pagination  page={page} onPageChange={changePage}/>
         <div className="table-responsive">
     <table className="table table-striped table-sm">
         <thead>
@@ -49,14 +52,11 @@ const DataTable = () => {
                 <td>{item.deals}</td>
                 <td>{item.amount.toFixed(2)}</td>
             </tr>
-
-            ))}
-        
-           
+        ))}
         </tbody>
     </table>
 </div>
-
+</>
     );
 }
 
